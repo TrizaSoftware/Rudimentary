20a0d0928a98ad7e7b74b9e35d25ef13bd6b162e
Jimmy <jimmehrblx@gmail.com>
2022-09-15 18:30:53 -0400

    Fix an error where "unnoclip" calls env.API.RemoteEvent

--- MainModule/Commands/Moderation/Unnoclip.lua
+++ MainModule/Commands/Moderation/Unnoclip.lua
@@ -8,7 +8,7 @@ Command.Schema = {{["Name"] = "User(s)", ["Type"] = "String"}}
 Command.ArgsToReplace = {1}
 Command.Handler = function(env : table, plr : Player, args : table)
     if not args[1] then
-        env.API.RemoteEvent:FireClient(plr, "showHint", {Title = "Error", Text = "You can't clip no one."})
+        env.RemoteEvent:FireClient(plr, "showHint", {Title = "Error", Text = "You can't clip no one."})
         env.RemoteEvent:FireClient(plr, "playSound", "Error")
         return
    end 
@@ -16,7 +16,7 @@ Command.Handler = function(env : table, plr : Player, args : table)
    for _, target : Player in args[1] do
         if typeof(target) == "Instance" then
             if not target.Character:FindFirstChild("RudimentaryNoClipHandler") then
-                env.API.RemoteEvent:FireClient(plr, "showHint", {Title = "Error", Text = string.format("%s isn't noclipping.", target.Name)})
+                env.RemoteEvent:FireClient(plr, "showHint", {Title = "Error", Text = string.format("%s isn't noclipping.", target.Name)})
                 env.RemoteEvent:FireClient(plr, "playSound", "Error")
             end
             target.Character.RudimentaryNoClipHandler:Destroy()
