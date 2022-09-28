1f0e30e96c7b2ebf6a555612119d21bfd0805f81
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2022-09-28 09:33:02 -0400

    Begin Work on Button Class

--- MainModule/Client/ClientLoader/Dependencies/Button.lua
+++ MainModule/Client/ClientLoader/Dependencies/Button.lua
@@ -1,8 +1,20 @@
 local Button = {}
+local Properties = {
+	["Color"] = function(self, color)
+
+	end
+}
+Button.__index = Button
+Button.__newindex = function(self, index, value)
+	assert(Properties[index], string.format("%s isn't a valid property.", index))
+	local suc, result = pcall(Properties[index], self, value)
+	if not suc then warn(result) else return result end
+end
 
 function Button.new()
-	local self = setmetatable({}, {__index ={}})
-	
+	local self = {}
+
+	return setmetatable(self, Button)
 end
 
 return Button
