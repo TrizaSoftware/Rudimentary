b29e26ad3fe14b3329abcd8e5485f13e29988e12
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-01-17 17:29:13 +0000

    Fix Args for Command Management

--- src/Server/init.lua
+++ src/Server/init.lua
@@ -855,7 +855,7 @@ local function handlePlayer(player)
 		end
 		mainTable.ClientLogs[player.UserId] = {}
 		local adminLevel = getAdminLevel(mainTable.GroupConfig, player)
-		if (mainTable.Admins[player.UserId] and adminLevel > mainTable.Admins[player.UserId]) or not mainTable.Admins[player.UserId] then
+		if not mainTable.Admins[player.UserId] or (mainTable.Admins[player.UserId] and adminLevel > mainTable.Admins[player.UserId]) then
 			mainTable.Admins[player.UserId] = adminLevel
 		end
 		--warn(string.format("Admin Level For %s Is: %s", player.Name, mainTable.Admins[player.UserId]))
