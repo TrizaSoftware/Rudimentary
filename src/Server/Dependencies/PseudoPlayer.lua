8aa44bb2816ae329596c9d7e709f46a6f4ee88ba
CodedJimmy <46508035+CodedJimmy@users.noreply.github.com>
2023-07-17 23:25:10 -0400

    Update PseudoPlayer

--- src/Server/Dependencies/PseudoPlayer.lua
+++ src/Server/Dependencies/PseudoPlayer.lua
@@ -8,70 +8,61 @@ local SharedHelpers = Folder.Shared.Helpers
 local BetterSignal = require(SharedPackages.BetterSignal)
 local TableHelper = require(SharedHelpers.TableHelper)
 
-export type PseudoPlayer = {
-  Player: Player,
-  AdminLevel: number,
-  Destroy: () -> nil,
-  _propertyChangedSignalThreads: {thread},
-  GetPropertyChangedSignal: () -> {
-  }
-}
-
 local PseudoPlayers = {}
 
 local PseudoPlayer = {}
 PseudoPlayer.__index = PseudoPlayer
 
 function PseudoPlayer.new(player: Player)
-  local self: PseudoPlayer = setmetatable({}, PseudoPlayer)
-  self.Player = player
-  self.AdminLevel = 0
-  self._propertyChangedSignalThreads = {}
-  PseudoPlayers[player] = self
-  return self
+	local self: typeof(PseudoPlayer) = setmetatable({}, PseudoPlayer)
+	self.Player = player
+	self.AdminLevel = 0
+	self._propertyChangedSignalThreads = {}
+	PseudoPlayers[player] = self
+	return self
 end
 
 function PseudoPlayer:GetPropertyChangedSignal(propertyName: string)
-  local CurrentProperty = self[propertyName]
-  local PropertySignal = BetterSignal.new()
+	local CurrentProperty = self[propertyName]
+	local PropertySignal = BetterSignal.new()
 
- local Thread = coroutine.create(function()
-    while true do
-      if typeof(self[propertyName]) == "table" then
-        if TableHelper:CloneDeep(self[propertyName]) ~= CurrentProperty then
-          CurrentProperty = TableHelper:CloneDeep(self[propertyName])
-          PropertySignal:Fire(CurrentProperty)
-        end
-      else
-        if self[propertyName] ~= CurrentProperty then
-          CurrentProperty = self[propertyName]
-          PropertySignal:Fire(CurrentProperty)
-        end
-      end
-      RunService.Heartbeat:Wait()
-    end
-  end)
+	local Thread = coroutine.create(function()
+		while true do
+			if typeof(self[propertyName]) == "table" then
+				if TableHelper:CloneDeep(self[propertyName]) ~= CurrentProperty then
+					CurrentProperty = TableHelper:CloneDeep(self[propertyName])
+					PropertySignal:Fire(CurrentProperty)
+				end
+			else
+				if self[propertyName] ~= CurrentProperty then
+					CurrentProperty = self[propertyName]
+					PropertySignal:Fire(CurrentProperty)
+				end
+			end
+			RunService.Heartbeat:Wait()
+		end
+	end)
 
-  coroutine.resume(Thread)
-  table.insert(self._propertyChangedSignalThreads, Thread)
+	coroutine.resume(Thread)
+	table.insert(self._propertyChangedSignalThreads, Thread)
 
-  return PropertySignal
+	return PropertySignal
 end
 
 function PseudoPlayer:Destroy()
-  for _, thread in self._propertyChangedSignalThreads do
-    coroutine.close(thread)
-  end
-  PseudoPlayers[self.Player] = nil
-  setmetatable(self, nil)
-  for property, _ in self do
-    self[property] = nil
-  end
-  self = nil
+	for _, thread in self._propertyChangedSignalThreads do
+		coroutine.close(thread)
+	end
+	PseudoPlayers[self.Player] = nil
+	setmetatable(self, nil)
+	for property, _ in self do
+		self[property] = nil
+	end
+	self = nil
 end
 
-function PseudoPlayer:GetPseudoPlayerFromPlayer(player: Player): PseudoPlayer
-  return PseudoPlayers[player]
+function PseudoPlayer:GetPseudoPlayerFromPlayer(player: Player): typeof(PseudoPlayer)
+	return PseudoPlayers[player]
 end
 
 return PseudoPlayer
