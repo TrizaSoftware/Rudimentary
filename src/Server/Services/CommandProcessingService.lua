8b9ac0707f1eb3dbc054c618b67fa3339025775b
Jimmy <46508035+Jyrezo@users.noreply.github.com>
2023-09-18 13:18:23 +0000

    Start Working on Command Processing

--- src/Server/Services/CommandProcessingService.lua
+++ src/Server/Services/CommandProcessingService.lua
@@ -13,13 +13,18 @@ local Environment
 
 local CommandProcessingService = Service.new("CommandProcessingService")
 
-function CommandProcessingService:ProcessCommand()
-    
+function CommandProcessingService:ProcessCommand(player: Player, ...)
+    local Prefix = Environment.MainVariables.Prefix
+
+    local Data = {...}
+    local CommandString: string = Data[1]
+	local CommandNoPrefix = Prefix:len() >= 1
+			and (CommandString:sub(1, Prefix:len()) == Prefix and CommandString:split(Prefix)[2])
+		or CommandString
 end
 
 function CommandProcessingService:Initialize(Env)
     Environment = Env
-
     Environment.API.AddNetworkEventCallback("execute", function(player, ...)
         self:ProcessCommand(player, ...)
     end)
